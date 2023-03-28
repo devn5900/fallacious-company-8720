2d23e8795a09c96e7e5a54cdd64e75b930c07d99
Devesh Kumar Mishra <94969888+devn5900@users.noreply.github.com>
2023-03-28 22:11:04 +0530

    Readme and Backend

---
+++ backend/models/products.model.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const prodSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  category: { type: String, required: true },
+  price: { type: Number, required: true },
+  design: { type: String, required: true },
+  image: { type: String, required: true },
+  type: { type: String, required: true },
+  offer: { type: String, required: true },
+  brand: { type: String, required: true },
+  discount: { type: String, required: true },
+  tags: {
+    type: Array,
+    default: [
+      {
+        type: "fit",
+        label: "OVERSIZED FIT",
+        bgColor: "rgba(82, 82, 82, 0.8)",
+        textColor: "#fff",
+      },
+    ],
+  },
+  description: { type: String, required: true },
+  rating: { type: String, required: true },
+});
+
+const prodModel = mongoose.model("products", prodSchema);
+
+module.exports = {
+  prodModel,
+};
