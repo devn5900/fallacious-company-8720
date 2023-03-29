96380780f1e9be3beb0d34db58481315aeb76dcd
Sajan Kumar <sajan99559@gmail.com>
2023-03-29 19:35:21 +0530

    hero-section

--- frontend/src/Pages/Index.jsx
+++ frontend/src/Pages/Index.jsx
@@ -1,7 +1,55 @@
 import React from "react";
-
+import "./Index.css"
 const Index = () => {
-  return <div>Index</div>;
+  return <div className="main">
+    <div className="container">
+      <div className="here-section">
+            <div className="hero">
+                <div>
+                  <img src="https://images.ctfassets.net/5de70he6op10/gB8j1vpg2pP2rpRpa75i9/312436ccf606776719785ef409420f46/LS_M0_Banner_a.jpg?w=630&q=80&fm=webp" alt="" />
+                </div>
+                <div>
+                  <img src="https://images.ctfassets.net/5de70he6op10/1NPjQQ97u8xeHrFHkXjrax/8e715eaa8cf02c6c9daba8dadd4b2d6a/LS_M0_Banner_b.jpg?w=630&q=80&fm=webp" alt="" />
+                </div>
+                <div>
+                  <img src="https://images.ctfassets.net/5de70he6op10/1Lyxv82WJDkGaRWROJFSL3/d2a3f0c2dcfc706a09c4b7991b43f67d/LS_M0_Banner_f.jpg?w=630&q=80&fm=webp" alt="" />
+                </div>
+                <div>
+                  <img src="https://images.ctfassets.net/5de70he6op10/5NZtjeqhlhchOW8aK8ALjQ/b50f60cb0cdcdaf3e1878c13aaea7907/LS_M0_Banner_e.jpg?w=630&q=80&fm=webp" alt="" />
+                </div>
+            </div>
+            <div className="card-1">
+              <div>
+              <img src="https://images.ctfassets.net/5de70he6op10/2qNPYihpSMixD9hklRMIOH/952d55b8268d66373b3bc3333b733c0b/LS_M1a-1-2.jpg?w=1784&q=80&fm=webp"  alt="" />
+              <div className="btn-1"><a href="">the Reformation Shop</a></div>
+              </div>
+                  <div >
+                  <img src="https://images.ctfassets.net/5de70he6op10/7qi0oKfnqUGDqWtXby6cva/876293b82a386f8bdbf49e4e1aa28100/484733007-hpg_m1-b.gif?w=428&q=80&fm=webp" alt="" />
+                  </div>
+            </div>
+            <div className="card-2">
+              <div>
+                <img src="https://images.ctfassets.net/5de70he6op10/3oxEDi87p07u8PDBRUWZQT/799ee882cf52081f2d78dd8d9f4dcc62/484733075-ls_m2a.jpg?w=660&q=80&fm=webp" alt="" />
+                <div className="btn-1"><a href="">Shop dresses</a></div>
+              </div>
+              <div>
+                <img src="https://images.ctfassets.net/5de70he6op10/3oxEDi87p07u8PDBRUWZQT/799ee882cf52081f2d78dd8d9f4dcc62/484733075-ls_m2a.jpg?w=660&q=80&fm=webp" alt="" />
+                <div className="btn-1"><a href="">Shop dresses</a></div>
+              </div>
+              <div>
+                <img src="https://images.ctfassets.net/5de70he6op10/3oxEDi87p07u8PDBRUWZQT/799ee882cf52081f2d78dd8d9f4dcc62/484733075-ls_m2a.jpg?w=660&q=80&fm=webp" alt="" />
+                <div className="btn-1"><a href="">Shop dresses</a></div>
+              </div>
+              <div>
+                <img src="https://images.ctfassets.net/5de70he6op10/3oxEDi87p07u8PDBRUWZQT/799ee882cf52081f2d78dd8d9f4dcc62/484733075-ls_m2a.jpg?w=660&q=80&fm=webp" alt="" />
+                <div className="btn-1"><a href="">Shop dresses</a></div>
+              </div>
+            </div>
+      </div>
+
+    </div>
+
+  </div>;
 };
 
 export default Index;
