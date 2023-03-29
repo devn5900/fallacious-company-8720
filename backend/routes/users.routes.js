4b09c00f022df7c874aabd5a54a4c30058b60afd
Devesh Kumar Mishra <94969888+devn5900@users.noreply.github.com>
2023-03-29 23:55:53 +0530

    Products, Users, Carts Route updated

--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -1,2 +1,16 @@
 const { Router } = require("express");
+const { regValid, logValid } = require("../middlewares/users.middlware");
+const { register, login } = require("../controllers/users.controller");
+
 const userRouter = Router();
+
+userRouter.post("/register", regValid, register);
+userRouter.post("/login", logValid, login);
+
+userRouter.all("*", (req, res) => {
+  res.status(404).send({ msg: "Not Found" });
+});
+
+module.exports = {
+  userRouter,
+};
