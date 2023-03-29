a416b4e97501d672012e1c0e2caed6545c195293
Sajan Kumar <sajan99559@gmail.com>
2023-03-29 20:49:13 +0530

    hero-section complete

--- frontend/src/Pages/Index.jsx
+++ frontend/src/Pages/Index.jsx
@@ -30,20 +30,48 @@ const Index = () => {
             <div className="card-2">
               <div>
                 <img src="https://images.ctfassets.net/5de70he6op10/3oxEDi87p07u8PDBRUWZQT/799ee882cf52081f2d78dd8d9f4dcc62/484733075-ls_m2a.jpg?w=660&q=80&fm=webp" alt="" />
-                <div className="btn-1"><a href="">Shop dresses</a></div>
+                <div className="btn-2"><a href="">shop dresses</a></div>
               </div>
               <div>
-                <img src="https://images.ctfassets.net/5de70he6op10/3oxEDi87p07u8PDBRUWZQT/799ee882cf52081f2d78dd8d9f4dcc62/484733075-ls_m2a.jpg?w=660&q=80&fm=webp" alt="" />
-                <div className="btn-1"><a href="">Shop dresses</a></div>
+                <img src="https://images.ctfassets.net/5de70he6op10/6m0is55tRAh4uMrxrsZwyb/5d42159121116c07584fca7d41dc5ac2/484733095-ls_m2d.jpg?w=660&q=80&fm=webp" alt="" />
+                <div className="btn-2"><a href="">shop new clothing</a></div>
               </div>
               <div>
-                <img src="https://images.ctfassets.net/5de70he6op10/3oxEDi87p07u8PDBRUWZQT/799ee882cf52081f2d78dd8d9f4dcc62/484733075-ls_m2a.jpg?w=660&q=80&fm=webp" alt="" />
-                <div className="btn-1"><a href="">Shop dresses</a></div>
+                <img src="https://images.ctfassets.net/5de70he6op10/55I6Ziwm1BLgmFHzuRJi9U/d5afd53ea4e04c32444f7e6994de3a26/484733090-ls_m2c.jpg?w=660&q=80&fm=webp" alt="" />
+                <div className="btn-2"><a href="">shop jewelry</a></div>
               </div>
               <div>
-                <img src="https://images.ctfassets.net/5de70he6op10/3oxEDi87p07u8PDBRUWZQT/799ee882cf52081f2d78dd8d9f4dcc62/484733075-ls_m2a.jpg?w=660&q=80&fm=webp" alt="" />
-                <div className="btn-1"><a href="">Shop dresses</a></div>
+                <img src="https://images.ctfassets.net/5de70he6op10/6m0is55tRAh4uMrxrsZwyb/5d42159121116c07584fca7d41dc5ac2/484733095-ls_m2d.jpg?w=660&q=80&fm=webp" alt="" />
+                <div className="btn-2"><a href="">shop Reformation</a></div>
               </div>
+            </div>
+            <div className="card-1">
+              <div>
+              <img src="https://images.ctfassets.net/5de70he6op10/1V3Atq484hyeMWCw1MVXas/12f900820a162ba443ed50c4fdfd42d3/484733140-ls_m3a.jpg?w=1756&q=80&fm=webp"  alt="" />
+              <div className="btn-1"><a href="">the Reformation Shop</a></div>
+              </div>
+                  <div >
+                  <img src="https://images.ctfassets.net/5de70he6op10/3rKgQlzbypafdDTvNckO2q/193350ddf772ecfdc9c89ee5879ff993/484733144-ls_m3b.jpg?w=856&q=80&fm=webp" alt="" />
+                  </div>
+            </div>
+            <div className="card-3">
+              <div>
+                
+                <img src="https://images.ctfassets.net/5de70he6op10/7cxMotLF5i9rrL92ndmypR/88e8ae5aa03038acc259dd104a9d2963/484733176-ls_m4a.jpg?w=1302&q=80&fm=webp" alt="" />
+                <div className="btn-2" style={{marginLeft:"220px"}}><a href="">Mother's Day Gifts</a></div>
+              </div>
+              <div>
+                <img src="https://images.ctfassets.net/5de70he6op10/716Ut6ZHZcpObdM9yRjLX8/ceb76d3813b83c516a7773b4749d9dcd/484733181-ls_m4b.jpg?w=1302&q=80&fm=webp" alt="" />
+                <div className="btn-2" style={{marginLeft:"220px"}}><a href="">shop bedding</a></div>
+              </div>
+
+            </div>
+            <div className="card-4">
+              <div>
+                <img src="https://images.ctfassets.net/5de70he6op10/2XUxhNlu24HaHTFRAghRS3/f30d5364574fc72b718e910f087b2685/484733194-ls_m5a.jpg?w=2640&q=80&fm=webp" alt="" />
+                <div className="btn-2" style={{marginLeft:"500px"}}><a href="">shop new clothing</a></div>
+              </div>
+
             </div>
       </div>
 
