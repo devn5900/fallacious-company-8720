84d07a0f827c34a8b18ba4cc355ba7d49a283b6c
Vedprakash <vedprakash4gs@gmail.com>
2023-03-30 22:06:06 +0530

    added login file

---
+++ frontend/src/Components/Authentication/login.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import { Box, Button, FormControl, FormLabel, Input } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
+export default function SignUp() {
+  const [name,setName] = useState("")
+  const [email,setEmail] = useState("")
+  const [password,setPassword] = useState("")
+  const payload={
+    email:email,
+    password:password,
+  }
+     const Userlogin=()=>{
+       console.log(payload)
+       fetch("https://clean-earrings-yak.cyclic.app/users/login",{
+        method:"POST",
+           headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify(payload)
+       }).then((res)=>res.json()).then((res)=>{
+      console.log(res)
+      localStorage.setItem("token",res.token)
+    }
+    ).catch((err)=>{
+        console.log(err)
+    })  
+  }
+ 
+  return (
+<>
+    <Box pt={"2rem"} pb={"2rem"} bg={"black"}  color={"dodgerblue"}>
+    <Box  boxShadow={"dark-lg"} width={"50%"} m={"auto"} bg="white"  p={"1rem"} borderRadius={"10px"}>
+
+  <FormLabel>
+    Email
+  </FormLabel>
+  <Input  value={email} onChange={(e)=>setEmail(e.target.value)}  placeholder="email"/>
+<FormLabel>
+Password
+</FormLabel>
+<Input  value={password} onChange={(e)=>setPassword(e.target.value)} placeholder="password"/>
+
+  <Button onClick={Userlogin} mt={"5px"} type="submit">Submit</Button>
+</Box>
+</Box>
+    </>
+  )
+}
