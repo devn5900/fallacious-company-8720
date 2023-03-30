a46d33d57cf13f253092b3df50564cf002635930
Sajan Kumar <sajan99559@gmail.com>
2023-03-30 21:11:57 +0530

    crousel-done

--- frontend/src/Pages/Index.jsx
+++ frontend/src/Pages/Index.jsx
@@ -1,7 +1,92 @@
 import React from "react";
 import "./Index.css"
 import {StarIcon} from '@chakra-ui/icons'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 const Index = () => {
+  const slider = [
+    {
+      id: 1,
+      img: "https://assets.tatacliq.com/medias/sys_master/images/45576216608798.jpg",
+    },
+    {
+      id: 2,
+      img: "https://assets.tatacliq.com/medias/sys_master/images/45576216674334.jpg",
+    },
+    {
+      id: 3,
+      img: "https://assets.tatacliq.com/medias/sys_master/images/45576216739870.jpg",
+    },
+    {
+      id: 4,
+      img: "https://assets.tatacliq.com/medias/sys_master/images/45576216805406.jpg",
+    },
+    {
+      id: 5,
+      img: "https://assets.tatacliq.com/medias/sys_master/images/45622605905950.jpg",
+    },
+    {
+      id: 6,
+      img: "https://assets.tatacliq.com/medias/sys_master/images/45622606037022.jpg",
+    },
+    {
+      id: 7,
+      img: "https://assets.tatacliq.com/medias/sys_master/images/45619580305438.jpg",
+    },
+    {
+      id: 8,
+      img: "https://assets.tatacliq.com/medias/sys_master/images/45619580436510.jpg",
+    },
+    {
+      id: 9,
+      img: "https://assets.tatacliq.com/medias/sys_master/images/45536791658526.jpg",
+    },
+    {
+      id: 10,
+      img: "https://assets.tatacliq.com/medias/sys_master/images/45536791789598.jpg",
+    },
+    {
+      id: 11,
+      img: "https://assets.tatacliq.com/medias/sys_master/images/45619571556382.jpg",
+    },
+    {
+      id: 12,
+      img: "https://assets.tatacliq.com/medias/sys_master/images/45619569983518.jpg",
+    },
+  ]
+  const settings2 = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return <div className="main">
     <div className="container">
       <div className="here-section">
@@ -75,7 +160,7 @@ const Index = () => {
             </div>
             <div className="top-rated">
               <h3>Top-Rated Picks</h3>
-              <hr />
+              <hr style={{marginTop:"8px"}}/>
             </div>
             <div className="card-5">
               
@@ -151,9 +236,99 @@ const Index = () => {
                      <p className="shop-link"><a href="">shop now</a></p>
                  </div>
               </div>
-              
-
             </div>
+            <div className="top-rated" style={{marginBottom:"30px"}}>
+              <h3>You May Also Like</h3>
+              <hr style={{marginTop:"8px"}}/>
+              <hr />
+            </div>
+            <div >
+              <Slider {...settings2}>
+              {slider.map((item) => (
+                <div key={item.id} className="slider" >
+                  <img className="slide1" src={item.img} alt="" />
+                </div>
+              ))}
+            </Slider>
+
+              </div>
+              <div className="top-rated" style={{marginBottom:"30px"}}>
+              <h3>Trending Now</h3>
+              <hr style={{marginTop:"8px"}}/>
+              <hr />
+            </div>
+              <div >
+              <Slider {...settings2}>
+              {slider.map((item) => (
+                <div key={item.id} className="slider" >
+                  <img className="slide1" src={item.img} alt="" />
+                </div>
+              ))}
+            </Slider>
+
+              </div>
+              <div className="top-rated" style={{marginBottom:"30px",textAlign:"center",letterSpacing:"1px",fontSize:"17px",marginTop:"50px"}}>
+              <h3>More to Explore</h3>
+              <hr style={{marginTop:"8px"}}/>
+              <hr />
+            </div>
+            <div className="card-6">
+              <div>
+              <div>
+                <img src="https://images.ctfassets.net/5de70he6op10/U2fCd9Ep6otJYrxWA6a0t/2a236b9ea21b5785d7e98cee0f11871e/MTE_1.jpg?w=856&q=80&fm=webp" alt="" />
+              </div>
+              <div className="detail">
+                <p className="heading">
+                New Chapter, Fresh Paige
+                </p>
+                <p className="disctiption">
+                Turn to the experts in flattering fits and flawlessly tailored pairs to start spring on the right note.
+                </p>
+                <p className="shop-link">
+                  shop Paijama jeans
+                </p>
+              </div>
+                
+              </div>
+              <div>
+              <div>
+                <img src="https://images.ctfassets.net/5de70he6op10/25UKbR1vHJpq6yriubNoSD/c1e9f1ba93a96c9b3d7ac869951de29c/MTE_2.jpg?w=856&q=80&fm=webp" alt="" />
+              </div>
+              <div className="detail">
+                <p className="heading">
+                The Beauty Edit: TikTok-Famous!
+                </p>
+                <p className="disctiption">
+                Make Beauty (have you tried their skin tint??) is just one of the brands you'll find in our curated collection of gone-viral TikTok faves.
+                </p>
+                <p className="shop-link">
+                  don't miss these must-haves
+                </p>
+              </div>
+                
+              </div>
+              <div>
+              <div>
+                <img src="https://images.ctfassets.net/5de70he6op10/4oNoIgRJVULLXu6o02FnFA/79760e323b6140b7fc68bb50638c8079/032023_HPG_MTE_3.jpg?w=856&q=80&fm=webp" alt="" />
+              </div>
+              <div className="detail">
+                <p className="heading">
+                celebrating
+International Women's Day
+                </p>
+                <p className="disctiption">
+                We're proud to support women-owned brands each and every day.
+                </p>
+                <p className="shop-link">
+                  discover new favorites
+                </p>
+              </div>
+                
+              </div>
+             
+               
+            </div>
+
       </div>
 
     </div>
