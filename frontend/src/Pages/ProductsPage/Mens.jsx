ddb73f630de333f3fdd4898f17287fa13514633d
kripa Shankar Pandey <kripapandey302@gmail.com>
2023-03-31 12:23:02 +0530

    filter added in mens page

--- frontend/src/Pages/ProductsPage/Mens.jsx
+++ frontend/src/Pages/ProductsPage/Mens.jsx
@@ -5,32 +5,48 @@ import SideBar from "../../Components/ProductPage/SideBar";
 import { getMensData } from "../../Redux/productReducer/action";
 import { useDispatch, useSelector } from "react-redux";
 import MensProductsCard from "../../Components/ProductPage/MensProductsCard";
+import { useLocation, useSearchParams } from "react-router-dom";
 const Mens = () => {
-  const dispatch = useDispatch();
+  // this hook will return you the url
+  const location = useLocation();
+  // console.log(location);
   const { data, isError, isLoading } = useSelector(
     (store) => store.productReducer
   );
+  const [searchParams] = useSearchParams();
+  const dispatch = useDispatch();
+  let obj = {
+    params: {
+      category: searchParams.getAll("category"),
+      // _sort: searchParams.get("order") && "price",
+      // _order: searchParams.get("order"),
+    },
+  };
+
+  React.useEffect(() => {
+    dispatch(getMensData(obj));
+  }, [location.search]);
+  ///
   console.log(data, "data redux");
-  useEffect(() => {
-    dispatch(getMensData());
-  }, [dispatch]);
+
   return (
     <Box>
       <Flex direction={["column", "row"]}>
         <Box
-          w={["100%", "20%", "15%"]}
-          ml={["50px", "15px"]}
+          w={["80%", "20%", "15%"]}
+          pl={["auto", "5"]}
+          mr={["auto", "auto"]}
           borderRight={["0px", "1px solid gray", "1px solid gray"]}
         >
           <SideBar />
         </Box>
         <Box
-          w={["100%", "80%", "85%"]}
+          w={["90%", "78%", "82%"]}
           display="grid"
           gridTemplateColumns={["1fr", "1fr 1fr", "1fr 1fr", "1fr 1fr 1fr"]}
-          ml={[5, 5, 10, 20, 20]}
-          mr={[5, 5, 10, 20, 20]}
-          gap="10"
+          ml={["auto", "auto"]}
+          mr={["auto", "auto"]}
+          gap="auto"
         >
           {data.length > 0 &&
             data.map((e) => <MensProductsCard key={e._id} {...e} />)}
--- frontend/src/Redux/productReducer/action.js
+++ frontend/src/Redux/productReducer/action.js
@@ -7,20 +7,22 @@ import {
 } from "./actionType";
 import axios from "axios";
 //Mens Data action function
-export const getMensData = (payload) => (dispatch) => {
+export const getMensData = (obj) => (dispatch) => {
   dispatch({ type: GET_DATA_REQUEST });
+  console.log("o", obj);
   axios
-    .get("https://vast-duck-coat.cyclic.app/products?type=Men")
+    .get("https://vast-duck-coat.cyclic.app/products?type=Men", obj)
     .then((res) =>
       dispatch({ type: GET_MENS_DATA_SUCESS, payload: res.data.data })
     )
     .catch((err) => dispatch({ type: GET_DATA_ERROR }));
 };
 //Womens Data action function
-export const getWomensData = (payload) => (dispatch) => {
+export const getWomensData = (obj) => (dispatch) => {
   dispatch({ type: GET_DATA_REQUEST });
+
   axios
-    .get("https://vast-duck-coat.cyclic.app/products?type=Men")
+    .get("https://vast-duck-coat.cyclic.app/products?type=Men", obj)
     .then((res) =>
       dispatch({ type: GET_WOMENS_DATA_SUCESS, payload: res.data.data })
     )
