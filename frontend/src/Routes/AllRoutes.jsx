62f5f3b823b27c5f8c6424956cee6c7c980ae381
Sachin Tewatia <tewatiasachin2001@gmail.com>
2023-04-02 13:09:09 +0530

    complete

--- frontend/src/Routes/AllRoutes.jsx
+++ frontend/src/Routes/AllRoutes.jsx
@@ -5,11 +5,8 @@ import Footer from "../Components/Footer";
 import Index from "../Pages/Index";
 import Mens from "../Pages/ProductsPage/Mens";
 import Cart from "../Pages/cartPage/cart";
-<<<<<<< HEAD
-import Checkout from "../Pages/checkoutPage/checkout";
-=======
 import Womens from "../Pages/ProductsPage/Womens";
->>>>>>> 0a5f541a051ba130853868c56e8d11f58136d3e6
+import Checkout from "../Pages/checkoutPage/checkout";
 const AllRoutes = () => {
   return (
     <>
@@ -19,11 +16,8 @@ const AllRoutes = () => {
         <Route path="/" element={<Index />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/mens" element={<Mens />} />
-<<<<<<< HEAD
         <Route path="/checkout" element={<Checkout />} />
-=======
         <Route path="/womens" element={<Womens />} />
->>>>>>> 0a5f541a051ba130853868c56e8d11f58136d3e6
       </Routes>
       <Footer />
     </>
